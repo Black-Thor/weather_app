d8d67330a70ca884d93114f87542918b12fe4ef8
Stéphane <stephane857@live.fr>
2022-03-01 17:32:51 +0100

    Page test , shared_preferences et API ajoutée sur la page newCity.dart , test fait avec plusieurs valeur

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'views/homePage.dart';
+import 'views/newCity.dart';
 
 void main() {
-
-  runApp(const MaterialApp(
+  runApp(MaterialApp(
     title: "Todo App",
-    home: HomePage(),
+    home: City(),
   ));
 }
--- lib/services/meteo_service.dart
+++ lib/services/meteo_service.dart
@@ -1,8 +1,8 @@
-import 'dart:_http';
 import 'dart:convert';
-import 'dart:html';
+import 'package:geocoding/geocoding.dart';
 import 'package:http/http.dart' as http;
 import 'package:weather_app/models/meteo.dart';
+import 'package:flutter/widgets.dart';
 
 // Future<List<Meteo>> getmeteoData() async {
 //   List<Meteo> meteoData = [];
@@ -28,14 +28,32 @@ import 'package:weather_app/models/meteo.dart';
 // );
 // }
 
-Future<List<Meteo>> getmeteoData() async {
-  List<Meteo> meteoData = [];
+Future cityRequest(cityEntries) async {
+  List<Location> locations;
+  if (cityEntries.runtimeType == String) {
+    locations = await locationFromAddress(cityEntries);
+  } else {
+    locations = await locationFromAddress(cityEntries.text.toString());
+  }
 
   Uri url = Uri.https("api.openweathermap.org", "/data/2.5/weather", {
-    'lat' : meteoData[0].coord?.lat.toString(),
-    'lon' : meteoData[0].coord?.lon.toString(),
-
+    'lat': locations[0].latitude.toString(),
+    'lon': locations[0].longitude.toString(),
+    'units': 'metric',
+    'lang': 'fr',
+    'appid': '109e23b902d8a46b4fcca288e80abc1d'
   });
 
-  return meteoData;
+  print(url);
+
+  var response = await http.get(url);
+
+  if (response.statusCode == 200) {
+    var jsonResponde = jsonDecode(response.body);
+    print(jsonResponde);
+    print(Meteo.fromJson(jsonResponde));
+    return Meteo.fromJson(jsonResponde);
+  } else {
+    print('Failed to get data');
+  }
 }
